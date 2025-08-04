585c50cd13d822bd8c828da90263af131be7e1b3
yubai <luoyi@mouji.net>
2025-08-04 12:47:41 +0800

    feat(web): 增加 `UiIconBox` 组件

--- registry/web/.playground/app/pages/components/icon.vue
+++ registry/web/.playground/app/pages/components/icon.vue
@@ -1,8 +1,21 @@
 <script lang="ts" setup>
+const icons = Object.keys(useAppConfig().weme.icons)
 </script>
 
 <template>
-  <Container name="UiIcon" description="The Icon component.">
-    <UiIcon name="search" />
+  <Container name="UiIcon" description="A component to display any icon from Iconify.">
+    <div class="flex-(~ col) gap-y-12">
+      <div class="grid-(~ cols-12) gap-12">
+        <UiIcon v-for="icon in icons" :key="icon" :name="icon" />
+      </div>
+    </div>
+  </Container>
+
+  <Container name="UiIconBox">
+    <div class="flex-(~ col) gap-y-12">
+      <div class="grid-(~ cols-12) gap-12">
+        <UiIconBox v-for="icon in icons" :key="icon" :name="icon" variant="soft" color="info" />
+      </div>
+    </div>
   </Container>
 </template>
---
+++ registry/web/uno.config.ts
@@ -0,0 +1,6 @@
+import { mergeConfigs } from 'unocss'
+import config from './.playground/.nuxt/uno.config'
+
+export default mergeConfigs([config, {
+
+}])

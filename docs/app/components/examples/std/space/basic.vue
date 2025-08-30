1b15b9d88a31b5899a71bebafae5f643cc084b99
yubai <luoyi@mouji.net>
2025-08-30 11:23:49 +0800

    feat(std): 增加 `UiSpace` 组件

---
+++ docs/app/components/examples/std/space/basic.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+import Button from '@registry/std/components/button/button.vue'
+import Space from '@registry/std/components/space/space.vue'
+</script>
+
+<template>
+  <Space>
+    <Button>Item 1</Button>
+    <Button>Item 2</Button>
+    <Button>Item 3</Button>
+  </Space>
+</template>
---
+++ docs/app/components/examples/std/space/separator.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import Button from '@registry/std/components/button/button.vue'
+import Space from '@registry/std/components/space/space.vue'
+</script>
+
+<template>
+  <Space>
+    <Button>Item 1</Button>
+    <Button>Item 2</Button>
+    <Button>Item 3</Button>
+
+    <template #separator>
+      <div class="h-auto">
+        /
+      </div>
+    </template>
+  </Space>
+</template>

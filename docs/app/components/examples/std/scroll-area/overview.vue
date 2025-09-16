31d296065c3d4b7bb31c007c37b18b03ca6c6ce9
yubai <luoyi@mouji.net>
2025-09-16 17:20:51 +0800

    docs(std): 增加 `UiScrollArea` 组件文档

---
+++ docs/app/components/examples/std/scroll-area/overview.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import ScrollArea from '@registry/std/components/scroll-area/scroll-area.vue'
+</script>
+
+<template>
+  <ScrollArea class="w-80 h-60 bg-neutral-3 rounded-lg">
+    <div class="p-4">
+      <p v-for="i in 10" :key="i">
+        {{ i }}
+      </p>
+    </div>
+  </ScrollArea>
+</template>

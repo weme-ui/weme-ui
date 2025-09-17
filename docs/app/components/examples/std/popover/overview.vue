cf903eb9c72ad9d4460a0c9c9e027702fae5b4a8
yubai <luoyi@mouji.net>
2025-09-17 16:56:20 +0800

    docs(std): 增加 `UiPopover` 组件

---
+++ docs/app/components/examples/std/popover/overview.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import Button from '@registry/std/components/button/button.vue'
+import Popover from '@registry/std/components/popover/popover.vue'
+</script>
+
+<template>
+  <Popover show-arrow as-child>
+    <template #trigger>
+      <Button>Open</Button>
+    </template>
+
+    <div>
+      This is the content.
+    </div>
+  </Popover>
+</template>

0f34df9b90c6a2f9e33dd5fa2712c4c5fee2d26f
yubai <luoyi@mouji.net>
2025-09-17 10:00:48 +0800

    feat(std): 增加 `UiChip` 组件

---
+++ registry/std/components/chip/chip.style.ts
@@ -0,0 +1,58 @@
+import type { VariantProps } from '~/utils/styles'
+import { createColorCompoundVariants, createColorVariants } from '#weme/utils/styles'
+import { createVariants } from '~/utils/styles'
+
+export const useChipStyle = createVariants({
+  slots: {
+    base: 'flex-(inline center) relative shrink-0',
+    chip: 'flex-(~ center) ring-(1 neutral-1) rounded-full leading-none select-none text-[11px] font-medium whitespace-nowrap z-popup',
+  },
+
+  variants: {
+    color: createColorVariants({ defaults: '' }),
+    variant: {
+      normal: { chip: 'p-1' },
+      icon: { chip: 'ring-0' },
+      dotted: { chip: 'size-2' },
+    },
+    position: {
+      tr: { chip: 'top-0 right-0' },
+      tl: { chip: 'top-0 left-0' },
+      br: { chip: 'bottom-0 right-0' },
+      bl: { chip: 'bottom-0 left-0' },
+    },
+    standalone: {
+      true: { chip: 'mr-1.5' },
+      false: { chip: 'abs' },
+    },
+    inset: {
+      false: '',
+    },
+  },
+
+  compoundVariants: [
+    ...createColorCompoundVariants({
+      type: 'static',
+      variants: { normal: 'solid', icon: 'link', dotted: 'solid' },
+      slot: 'chip',
+    }),
+
+    { variant: ['normal', 'icon'], class: { chip: 'min-w-4 h-4' } },
+
+    { inset: false, standalone: false, position: 'tr', class: { chip: '-translate-y-1/2 translate-x-1/2' } },
+    { inset: false, standalone: false, position: 'tl', class: { chip: '-translate-y-1/2 -translate-x-1/2' } },
+    { inset: false, standalone: false, position: 'br', class: { chip: 'translate-y-1/2 translate-x-1/2' } },
+    { inset: false, standalone: false, position: 'bl', class: { chip: 'translate-y-1/2 -translate-x-1/2' } },
+  ],
+
+  defaultVariants: {
+    color: 'accent',
+    variant: 'normal',
+    position: 'tr',
+    standalone: false,
+    inset: false,
+  },
+})
+
+export type ChipStyleSlots = typeof useChipStyle['slots']
+export type ChipStyleProps = VariantProps<typeof useChipStyle>
---
+++ registry/std/components/chip/chip.vue
@@ -0,0 +1,53 @@
+<script lang="ts" setup>
+import type { ChipProps } from './chip.props'
+import { reactivePick } from '@vueuse/core'
+import { Primitive } from 'reka-ui'
+import { computed } from 'vue'
+import { cn } from '~/utils/styles'
+import { useChipStyle } from './chip.style'
+
+const props = withDefaults(defineProps<ChipProps>(), {
+  color: 'accent',
+  variant: 'normal',
+  position: 'tr',
+  standalone: false,
+  inset: false,
+  visible: true,
+  hideZero: false,
+})
+
+const isVisible = computed(() => {
+  if (!props.visible)
+    return false
+
+  if (props.hideZero)
+    return Number(props.value) > 0
+
+  return props.visible
+})
+
+const showValue = computed(() => {
+  return Object.prototype.toString.call(props.value).slice(8, -1) === 'Number' && props.maxValue
+    ? `${Math.min(Number(props.value), props.maxValue)}+`
+    : props.value
+})
+
+const delegated = reactivePick(props, 'as', 'asChild')
+const ui = computed(() => useChipStyle({
+  ...props,
+  standalone: !!props.standalone,
+  inset: !!props.inset,
+}))
+</script>
+
+<template>
+  <Primitive v-bind="delegated" :class="cn(ui.base(), props.class, props.ui?.base)">
+    <span v-if="isVisible" :class="cn(ui.chip(), props.class, props.ui?.chip)">
+      <slot name="value">
+        {{ showValue }}
+      </slot>
+    </span>
+
+    <slot v-bind="$attrs" />
+  </Primitive>
+</template>

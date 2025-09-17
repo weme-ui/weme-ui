f83a92519ee0ff89b66985ad3a1c904cf5a98f9d
yubai <luoyi@mouji.net>
2025-09-17 09:14:59 +0800

    feat(unocss-preset): 增加 `animate-marquee`

--- packages/unocss-preset/src/theme/animation.ts
+++ packages/unocss-preset/src/theme/animation.ts
@@ -5,17 +5,26 @@ export const animation = {
     // Collapse
     'collapse-open': '{0%{height:0}100%{height:var(--reka-collapsible-content-height)}}',
     'collapse-close': '{0%{height:var(--reka-collapsible-content-height)}100%{height:0}}',
+
+    // Marquee
+    'marquee': '{0%{transform:translate(-100%) skew(-15deg)}100%{transform:translate(100%) skew(-15deg)}}',
   },
 
   durations: {
     // Collapse
     'collapse-open': '0.2s',
     'collapse-close': '0.2s',
+
+    // Marquee
+    'marquee': '2s',
   },
 
   timingFns: {
     // Collapse
     'collapse-open': 'ease-out',
     'collapse-close': 'ease-out',
+
+    // Marquee
+    'marquee': 'linear',
   },
 } satisfies Theme['animation']

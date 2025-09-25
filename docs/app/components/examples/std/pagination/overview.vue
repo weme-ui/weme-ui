d8ac115cea4fdb976d85cf0828db8d5f1606e6d2
yubai <luoyi@mouji.net>
2025-09-25 14:42:22 +0800

    feat(std): 增加 `UiPagination` 组件及文档

---
+++ docs/app/components/examples/std/pagination/overview.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import Pagination from '@registry/std/components/pagination/pagination.vue'
+
+const page = ref(5)
+</script>
+
+<template>
+  <Pagination v-model:page="page" :total="100" show-total show-more />
+</template>

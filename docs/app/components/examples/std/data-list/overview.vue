498d2277b685134500c63be805abb6bc22ab8a17
yubai <luoyi@mouji.net>
2025-09-29 10:37:21 +0800

    feat(std): 增加 `UiDataList` 组件及文档

---
+++ docs/app/components/examples/std/data-list/overview.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import DataList from '@registry/std/components/data-list/data-list.vue'
+
+const items = [
+  { label: 'Name', value: 'John Doe' },
+  { label: 'Email', value: 'john.doe@example.com' },
+  { label: 'Phone', value: '123-456-7890' },
+]
+</script>
+
+<template>
+  <DataList size="sm" orientation="vertical" :items="items" class="grid-cols-3" highlight />
+</template>

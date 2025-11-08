c369b8cc75bf05c6ebfd20388a5313875414041f
yubai <luoyi@mouji.net>
2025-11-08 09:44:32 +0800

    feat(cli): `build` 时更新 `registry` 版本

--- packages/cli/src/commands/build/manifest.ts
+++ packages/cli/src/commands/build/manifest.ts
@@ -5,12 +5,14 @@ import { DEFAULT_REGISTRY_PREFIX, MANIFEST_FILE_NAME, manifestSchema, REGISTRY_F
 import chalk from 'chalk'
 import path from 'pathe'
 import { glob } from 'tinyglobby'
+import { loadPackageJson } from '../../utils/schema/package'
 import { loadRegistryConfig, REGISTRY_DIRNAME } from '../../utils/schema/registry'
 
 export default async function (args: Record<string, any>) {
   p.intro(chalk.bold.blue('✨ Building Weme UI Repository'))
 
   const registries = await loadRegistries(args.cwd)
+  await updateRegistries(args.cwd, registries)
   await updateManifest(args.cwd, registries)
 
   p.outro('🥤 All done!')
@@ -32,12 +34,13 @@ async function loadRegistries(cwd: string): Promise<ManifestSchema[]> {
     (acc, file) => {
       const directory = path.dirname(file)
       const config = loadRegistryConfig(path.resolve(cwd, REGISTRY_DIRNAME, directory))
+      const pkg = loadPackageJson(path.resolve(cwd, REGISTRY_DIRNAME, directory))
 
       if (config.isOk()) {
         const result = manifestSchema.safeParse({
           name: config.unwrap().name,
           description: config.unwrap().description,
-          version: config.unwrap().version || '0.0.0',
+          version: pkg.unwrap().version || '0.0.0',
           directory: path.join(REGISTRY_DIRNAME, directory),
           prefix: config.unwrap().prefix || DEFAULT_REGISTRY_PREFIX,
           access: config.unwrap().access || 'public',
@@ -65,6 +68,25 @@ async function loadRegistries(cwd: string): Promise<ManifestSchema[]> {
   spinner.stop('Registries loaded!')
 
   return registries
+}
+
+async function updateRegistries(cwd: string, registries: ManifestSchema[]) {
+  const spinner = p.spinner()
+
+  spinner.start('Updating registries...')
+
+  registries.forEach((reg) => {
+    const registry = loadRegistryConfig(path.resolve(cwd, reg.directory)).unwrap()
+
+    registry.version = reg.version
+
+    fs.writeFileSync(
+      path.resolve(cwd, reg.directory, REGISTRY_FILE_NAME),
+      `${JSON.stringify(registry, null, 2)}\n`,
+    )
+  })
+
+  spinner.stop('Registries updated!')
 }
 
 async function updateManifest(cwd: string, registries: ManifestSchema[]) {

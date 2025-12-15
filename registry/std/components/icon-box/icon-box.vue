0bae9263803b4addebd00c564167d51a06c61ba1
yubai <luoyi@mouji.net>
2025-12-15 11:00:25 +0800

    fix(std): `class` 穿透至子组件

--- registry/std/components/icon-box/icon-box.vue
+++ registry/std/components/icon-box/icon-box.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import type { IconBoxProps } from './icon-box.props'
-import { reactivePick } from '@vueuse/core'
+import { reactiveOmit, reactivePick } from '@vueuse/core'
 import { Primitive } from 'reka-ui'
 import { computed } from 'vue'
 import { cn } from '~/utils/styles'
@@ -15,13 +15,14 @@ const props = withDefaults(defineProps<IconBoxProps>(), {
 })
 
 const delegated = reactivePick(props, 'as', 'asChild')
+const iconProps = reactiveOmit(props, 'as', 'asChild', 'ui', 'class')
 const ui = computed(() => useIconBoxStyle(props))
 </script>
 
 <template>
   <Primitive v-bind="delegated" :class="cn(ui.base(), props.ui?.base, props.class)">
     <slot>
-      <Icon v-bind="props" :class="cn(ui.icon(), props.ui?.icon)" />
+      <Icon v-bind="iconProps" :class="cn(ui.icon(), props.ui?.icon)" />
     </slot>
   </Primitive>
 </template>

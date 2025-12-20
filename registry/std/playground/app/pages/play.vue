12c613d50b9b24cd380f99759c2155438da9e66c
yubai <luoyi@mouji.net>
2025-12-20 10:42:54 +0800

    feat(std): 增加 `defineShortcuts()` 组合式函数

--- registry/std/playground/app/pages/play.vue
+++ registry/std/playground/app/pages/play.vue
@@ -1,10 +1,24 @@
 <script lang="ts" setup>
+const { add } = useToast()
+
+defineShortcuts({
+  c: addInfo,
+})
+
+function addInfo() {
+  add({
+    title: 'Info',
+    description: 'This is an info toast.',
+  })
+}
 </script>
 
 <template>
   <Container name="PLAY YOUR CODE HERE" description="🐗 Hakuna matata">
-    <UiToggleTooltip text="Hello" loading>
-      <UiIconButton icon="info" variant="plain" size="sm" />
-    </UiToggleTooltip>
+    <UiToastProvider>
+      <p class="text-sm">
+        Press <UiKbd>c</UiKbd> to show toast.
+      </p>
+    </UiToastProvider>
   </Container>
 </template>

5d9e97dfbfb440bf14130d1d8d50ef6f361a9027
yubai <luoyi@mouji.net>
2025-12-25 19:33:57 +0800

    feat(std): `UiAspectRatio` 增加示例

---
+++ registry/std/components/aspect-ratio/examples/basic.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import AspectRatio from '../aspect-ratio.vue'
+</script>
+
+<template>
+  <div class="mx-auto relative w-90 overflow-hidden rounded-lg">
+    <AspectRatio :ratio="16 / 9">
+      <img src="https://images.unsplash.com/photo-1498855926480-d98e83099315?w=300&dpr=2&q=80" class="size-full object-cover">
+    </AspectRatio>
+  </div>
+</template>
---
+++ registry/std/playground/app/pages/examples/layout/aspect-ratio.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import AspectRatioExampleBasic from '@registry/components/aspect-ratio/examples/basic.vue'
+</script>
+
+<template>
+  <div class="mb-4">
+    <AspectRatioExampleBasic />
+  </div>
+</template>

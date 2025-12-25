3c8b70e45f3af5beba4226e032bc44fd38627a52
yubai <luoyi@mouji.net>
2025-12-25 09:40:02 +0800

    feat(std): `UiIcon` 增加示例

---
+++ registry/std/components/icon/examples/basic.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import Tooltip from '../../tooltip/tooltip.vue'
+import Icon from '../icon.vue'
+
+const config = useAppConfig()
+const icons = Object.keys(config.weme.icons)
+</script>
+
+<template>
+  <div class="grid-(~ cols-15) h-full py-4 gap-6">
+    <template v-for="icon in icons" :key="icon">
+      <Tooltip :text="icon">
+        <Icon :name="icon" />
+      </Tooltip>
+    </template>
+  </div>
+</template>

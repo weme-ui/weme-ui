9a86aa96101cc7234afd4906a416d4ef57fac9ba
yubai <luoyi@mouji.net>
2025-12-25 19:34:32 +0800

    feat(std): `UiFloat` 增加示例

---
+++ registry/std/components/float/examples/basic.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import Float from '../float.vue'
+</script>
+
+<template>
+  <div class="relative size-12 rounded-lg bg-muted">
+    <Float>
+      <div class="rounded-full size-3 bg-primary" />
+    </Float>
+  </div>
+</template>
---
+++ registry/std/components/float/examples/with-placement.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import Float from '../float.vue'
+
+const placements = [
+  'top-start',
+  'top-center',
+  'top-end',
+  'bottom-start',
+  'bottom-center',
+  'bottom-end',
+  'middle-start',
+  'middle-center',
+  'middle-end',
+] as const
+</script>
+
+<template>
+  <div class="grid-(~ cols-3) gap-6 h-full py-4 gap-6">
+    <template v-for="placement in placements" :key="placement">
+      <div class="relative size-12 rounded-lg bg-muted mx-auto">
+        <Float :placement="placement">
+          <div class="flex rounded-full p-1.5 bg-danger" />
+        </Float>
+      </div>
+    </template>
+  </div>
+</template>

99fa40a50dd83205bee3e931161c35537511be93
yubai <luoyi@mouji.net>
2026-01-15 17:21:14 +0800

    fix(std): `usePortal()` `disabled` 始终返回 `true`

--- registry/std/composables/use-portal.ts
+++ registry/std/composables/use-portal.ts
@@ -7,7 +7,7 @@ export function usePortal(portal: Ref<boolean | string | HTMLElement | undefined
   const globalPortal = inject(portalContextInjectKey, undefined)
 
   const value = computed(() => portal.value === true ? globalPortal?.value : portal.value)
-  const disabled = computed(() => typeof value.value === 'boolean' && value.value === false)
+  const disabled = computed(() => typeof value.value === 'boolean' ? !value.value : false)
   const to = computed(() => typeof value.value === 'boolean' ? 'body' : value.value)
 
   return computed(() => ({
